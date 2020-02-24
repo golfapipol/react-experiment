51d011470915ee676f45b21da5bb7c50c8b0025b
Apipol Sukgler <golf.apipol@gmail.com>
2020-02-24 15:49:49 +0700

    [Create]: เปรียบเทียบ nextjs กับ react + router

---
+++ nextjs/pages/_app.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import App from 'next/app';
+import Link from 'next/link'
+
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    const pageProps = Component.getInitialProps
+      ? await Component.getInitialProps(ctx)
+      : {}
+    return { pageProps }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Component {...pageProps} />
+      </div>
+    );
+  }
+}
+
+export default MyApp;
